f1b23df46ffe904cab578f7e5d3404aa2e7c4382
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-04-29 15:57:51 -0400

    Changed image repository to GPTE DevOps Automation Quay Organization

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 VERSION=v0.0.7
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/userquota-operator:${VERSION}
-docker push quay.io/wkulhanek/userquota-operator:${VERSION}
+operator-sdk build quay.io/gpte-devops-automation/userquota-operator:${VERSION}
+docker push quay.io/gpte-devops-automation/userquota-operator:${VERSION}
